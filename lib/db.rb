ae13cdd7b53272f16596b4efc16bfef394922ad3
odradek <odradek@rlab.be>
2021-11-12 11:26:42 +0000

    add db

--- lib/db.rb
+++ lib/db.rb
@@ -4,8 +4,10 @@ require 'sqlite3'
 class Db
   def initialize
 
+    time = Time.now
     db = SQLite3::Database.open 'bot.db'
-    db.execute "CREATE TABLE IF NOT EXISTS records(weight INT, date DATE)"
+    db.execute "CREATE TABLE IF NOT EXISTS records(weight INT, date TEXT)"
+    db.execute "INSERT INTO records (weight, date ) VALUES (?, ?)", 'input', 'time'
 
   end
 end
